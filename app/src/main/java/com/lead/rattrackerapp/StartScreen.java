dcde0113b33120b6382e139f90023ee473381811
joeyjackson <joeyjackson4712@gmail.com>
2017-09-21 21:54:44 -0400

    bug fix

--- app/src/main/java/com/lead/rattrackerapp/StartScreen.java
+++ app/src/main/java/com/lead/rattrackerapp/StartScreen.java
@@ -2,6 +2,7 @@ package com.lead.rattrackerapp;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,8 +14,8 @@ import android.widget.Toast;
 public class StartScreen extends AppCompatActivity {
     Button loginButton;
     Button signUpButton;
-    EditText emailInput;
-    EditText passwordInput;
+    TextInputEditText emailInput;
+    TextInputLayout passwordInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +23,10 @@ public class StartScreen extends AppCompatActivity {
         setContentView(R.layout.activity_start_screen);
         loginButton = (Button) findViewById(R.id.loginButton);
         signUpButton = (Button) findViewById(R.id.signUpButton);
-        emailInput = (EditText) findViewById(R.id.input_email);
-        passwordInput = (EditText) findViewById(R.id.input_password);
+        emailInput = (TextInputEditText) findViewById(R.id.input_email);
+        passwordInput = (TextInputLayout) findViewById(R.id.input_password);
 
-        final TextInputLayout password = (TextInputLayout) findViewById(R.id.input_password_layout);
-        password.setPasswordVisibilityToggleEnabled(true);
+        passwordInput.setPasswordVisibilityToggleEnabled(true);
 
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,12 +39,13 @@ public class StartScreen extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (checkUsernamePassword(emailInput.getText().toString(), passwordInput.getText().toString())) {
+                if (checkUsernamePassword(emailInput.getText().toString(),
+                        passwordInput.getEditText().getText().toString())) {
                     Intent intent = new Intent(StartScreen.this, SignUpScreen.class);
                     startActivity(intent);
                 } else {
                     emailInput.setText("");
-                    passwordInput.setText("");
+                    passwordInput.getEditText().setText("");
                     Toast toast = Toast.makeText(getApplicationContext(),
                             "Username and Password were not correct", Toast.LENGTH_LONG);
                     toast.show();
