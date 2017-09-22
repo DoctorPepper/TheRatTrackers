7a927df8c4a0750b9d8d47889fe0a9c332485365
Julian Mancini <julian.mancini20@gmail.com>
2017-09-21 20:50:58 -0400

    Add slightly updated UI -- buttons to be updates

--- app/src/main/java/com/lead/rattrackerapp/StartScreen.java
+++ app/src/main/java/com/lead/rattrackerapp/StartScreen.java
@@ -1,13 +1,41 @@
 package com.lead.rattrackerapp;
 
+import android.content.Intent;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartScreen extends AppCompatActivity {
+    Button loginButton;
+    Button signUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
+        loginButton = (Button) findViewById(R.id.loginButton);
+        signUpButton = (Button) findViewById(R.id.signUpButton);
+
+        TextInputLayout password = (TextInputLayout) findViewById(R.id.input_password_layout);
+        password.setPasswordVisibilityToggleEnabled(true);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartScreen.this, LogInScreen.class);
+                startActivity(intent);
+            }
+        });
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartScreen.this, SignUpScreen.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
