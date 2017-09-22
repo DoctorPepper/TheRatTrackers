d78b500c74f8038649ef37077a1d4bf4e84ad1a7
joeyjackson <joeyjackson4712@gmail.com>
2017-09-21 21:48:16 -0400

    added check username and password functionality to login button

--- app/src/main/java/com/lead/rattrackerapp/StartScreen.java
+++ app/src/main/java/com/lead/rattrackerapp/StartScreen.java
@@ -1,15 +1,20 @@
 package com.lead.rattrackerapp;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class StartScreen extends AppCompatActivity {
     Button loginButton;
     Button signUpButton;
+    EditText emailInput;
+    EditText passwordInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +22,10 @@ public class StartScreen extends AppCompatActivity {
         setContentView(R.layout.activity_start_screen);
         loginButton = (Button) findViewById(R.id.loginButton);
         signUpButton = (Button) findViewById(R.id.signUpButton);
+        emailInput = (EditText) findViewById(R.id.input_email);
+        passwordInput = (EditText) findViewById(R.id.input_password);
 
-        TextInputLayout password = (TextInputLayout) findViewById(R.id.input_password_layout);
+        final TextInputLayout password = (TextInputLayout) findViewById(R.id.input_password_layout);
         password.setPasswordVisibilityToggleEnabled(true);
 
         signUpButton.setOnClickListener(new View.OnClickListener() {
@@ -29,5 +36,31 @@ public class StartScreen extends AppCompatActivity {
             }
         });
 
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkUsernamePassword(emailInput.getText().toString(), passwordInput.getText().toString())) {
+                    Intent intent = new Intent(StartScreen.this, SignUpScreen.class);
+                    startActivity(intent);
+                } else {
+                    emailInput.setText("");
+                    passwordInput.setText("");
+                    Toast toast = Toast.makeText(getApplicationContext(),
+                            "Username and Password were not correct", Toast.LENGTH_LONG);
+                    toast.show();
+                }
+
+            }
+        });
+
+    }
+
+    public boolean checkUsernamePassword(String username, String password) {
+        //can change this later to actually do it right
+        if (username.equals("user") && password.equals("password")) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
