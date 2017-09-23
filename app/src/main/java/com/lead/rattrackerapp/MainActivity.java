2625068668c0b2465c5d765e50054d1a73b38407
Julian Mancini <julian.mancini20@gmail.com>
2017-09-23 17:30:18 -0400

    Add log out button to main activity, minor style fixes.

--- app/src/main/java/com/lead/rattrackerapp/MainActivity.java
+++ app/src/main/java/com/lead/rattrackerapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.lead.rattrackerapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button logOutButton = (Button) findViewById(R.id.button_log_out);
+
+        logOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, StartScreen.class);
+                startActivity(intent);
+            }
+        });
     }
 }
